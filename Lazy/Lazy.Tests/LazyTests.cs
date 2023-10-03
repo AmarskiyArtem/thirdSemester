647182167efb4af5546b5860343534be7e039063
Artem <amarskiywork@gmail.com>
2023-10-03 13:21:57 +0300

    make parallel test better

--- Lazy/Lazy.Tests/LazyTests.cs
+++ Lazy/Lazy.Tests/LazyTests.cs
@@ -23,15 +23,22 @@ public class Tests
         var lazy = new LazyMultithreading<int>(Func);
         var list = new List<int>();
         var threads = new Thread[8];
+        var threadHandler = new ManualResetEvent(false);
         for (var i = 0; i < threads.Length; ++i)
         {
-            threads[i] = new Thread(() => list.Add(lazy.Get()));
+            threads[i] = new Thread(() =>
+            {
+                threadHandler.WaitOne();
+                list.Add(lazy.Get());
+            });
         }
-
+        
         foreach (var thread in threads)
         {
             thread.Start();
         }
+        
+        threadHandler.Set();
         
         foreach (var thread in threads)
         {
