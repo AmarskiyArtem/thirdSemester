75a38d6a90c2be19dff41830721f454687d774e4
Artem <amarskiywork@gmail.com>
2023-10-17 22:25:07 +0300

    wip

--- ThreadPool/MyThreadPool/MyThreadPool.cs
+++ ThreadPool/MyThreadPool/MyThreadPool.cs
@@ -1,6 +1,12 @@
 namespace ThreadPool;
 
+using System.Threading;
+
 public class MyThreadPool
 {
-    
+    public int ThreadCount { get;}
+    public MyThreadPool(int threadCount)
+    {
+        ThreadCount = threadCount;
+    }
 }
