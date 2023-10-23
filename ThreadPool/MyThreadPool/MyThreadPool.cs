46d15b211c66cc81d8d7529eeede6030c8d28290
Artem <amarskiywork@gmail.com>
2023-10-23 14:48:36 +0300

    wip

--- ThreadPool/MyThreadPool/MyThreadPool.cs
+++ ThreadPool/MyThreadPool/MyThreadPool.cs
@@ -4,6 +4,9 @@ using System.Threading;
 
 public class MyThreadPool
 {
+    private readonly Queue<Action> _tasks;
+    private readonly 
+    
     public int ThreadCount { get;}
     public MyThreadPool(int threadCount)
     {
