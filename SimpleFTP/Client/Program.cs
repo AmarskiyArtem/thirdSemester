fde35d235a9e1f139668b702c7208690a6e0de56
Artem <amarskiywork@gmail.com>
2023-10-25 22:07:01 +0300

    add base client-server implementation

---
+++ SimpleFTP/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        string serverIP = "127.0.0.1";
+        int serverPort = 7000;
+        TcpClient client = new TcpClient(serverIP, serverPort);
+        NetworkStream stream = client.GetStream();
+
+        while (true)
+        {
+            Console.Write("Введите сообщение: ");
+            string message = Console.ReadLine();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+            data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            string response = Encoding.Unicode.GetString(data, 0, bytes);
+            Console.WriteLine("Сервер: " + response);
+        }
+
+        client.Close();
+    }
+}
---
+++ SimpleFTP/FTPServer/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main()
+    {
+        int port = 7000;
+        TcpListener server = new TcpListener(IPAddress.Any, 7000);
+
+        server.Start();
+        Console.WriteLine("Сервер запущен. Ожидание подключения клиента...");
+
+        TcpClient client = server.AcceptTcpClient();
+        Console.WriteLine("Клиент подключен.");
+
+        NetworkStream stream = client.GetStream();
+        byte[] data = new byte[256];
+        int bytes;
+
+        while (true)
+        {
+            bytes = stream.Read(data, 0, data.Length);
+            string message = Encoding.Unicode.GetString(data, 0, bytes);
+            Console.WriteLine("Клиент: " + message);
+
+            string response = "Сервер получил сообщение: " + message;
+            data = Encoding.Unicode.GetBytes(response);
+            stream.Write(data, 0, data.Length);
+        }
+
+        client.Close();
+        server.Stop();
+    }
+}
