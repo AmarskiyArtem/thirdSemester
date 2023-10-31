676d8d718f6c604044f1a779a49447e3ab844cd3
Artem <amarskiywork@gmail.com>
2023-10-31 23:20:29 +0300

    wip

--- SimpleFTP/FTPServer/Server.cs
+++ SimpleFTP/FTPServer/Server.cs
@@ -16,18 +16,32 @@ class Server
         Console.WriteLine("Клиент подключен.");
 
         NetworkStream stream = client.GetStream();
-        byte[] data = new byte[256];
+        byte[] data = new byte[client.ReceiveBufferSize];
         int bytes;
 
         while (true)
         {
             bytes = stream.Read(data, 0, data.Length);
             string message = Encoding.Unicode.GetString(data, 0, bytes);
-            Console.WriteLine("Клиент: " + message);
-
-            string response = "Сервер получил сообщение: " + message;
+            //Console.WriteLine("Клиент: " + message);
+            string response = "";
+            if (message.StartsWith("List"))
+            {
+                Console.WriteLine(message.Split(' ')[1]);
+                var files = Directory.GetFiles(message.Split(' ')[1]);
+                var directories = Directory.GetDirectories(message.Split(' ')[1]);
+                foreach (var file in files)
+                {
+                    response += file + "\n";
+                }
+                foreach (var file in directories)
+                {
+                    response += file + "\n";
+                }
+            }
             data = Encoding.Unicode.GetBytes(response);
             stream.Write(data, 0, data.Length);
+            stream.Flush();
         }
 
         client.Close();
