0693e09128714290c66f5fd54926e2d37ac4c58a
Artem <amarskiywork@gmail.com>
2023-11-05 15:08:54 +0300

    add base GetAsync and ListAsync implementations

--- SimpleFTP/Client/Client.cs
+++ SimpleFTP/Client/Client.cs
@@ -1,5 +1,5 @@
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
+using System.Text;
 
 namespace Client;
 
@@ -14,7 +14,7 @@ public class Client : IDisposable
         _client = new TcpClient(_serverIp, port);
     }
 
-    public void Start()
+    /*public void Start()
     {
         var stream = _client.GetStream();
         while (true)
@@ -22,7 +22,28 @@ public class Client : IDisposable
             Console.WriteLine("Write request: ");
             var request = Console.ReadLine();
         }
+    }*/
+
+    public async Task<byte[]> GetAsync(string path, Stream stream)
+    {
+        await stream.WriteAsync("2"u8.ToArray());
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(path));
+        await stream.WriteAsync("\n"u8.ToArray());
+        await stream.FlushAsync();
+        var buffer = new byte[_client.ReceiveBufferSize];
+        await stream.ReadAsync(buffer);
+        return buffer;
     }
+
+    public async Task<string> ListAsync(string path, Stream stream)
+    {
+        await stream.WriteAsync("1"u8.ToArray());
+        await stream.WriteAsync(Encoding.UTF8.GetBytes(path));
+        await stream.WriteAsync("\n"u8.ToArray());
+        var buffer = new byte[_client.ReceiveBufferSize];
+        var data = await stream.ReadAsync(buffer);
+        return Encoding.UTF8.GetString(buffer, 0, data);
+    } 
     
     public void Dispose()
     {
