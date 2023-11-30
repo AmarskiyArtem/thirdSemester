016acb4be7a9b4f8fd15e5f5f3c2ec1fae8ae240
Artem <amarskiywork@gmail.com>
2023-11-30 17:10:03 +0300

    complete task

---
+++ MD5/MD5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using MD5;
+
+var path = args[0];
+try
+{
+    var stopWatch = new Stopwatch();
+    stopWatch.Start();
+    var result = CheckSum.ComputeCheckSumParallel(path);
+    stopWatch.Stop();
+    Console.WriteLine($"Check sum for {path} using not parallel method");
+    Console.WriteLine(BitConverter.ToString(result));
+    Console.WriteLine($"Time spent {stopWatch.Elapsed}");
+    stopWatch.Reset();
+    stopWatch.Start();
+    result = CheckSum.ComputeCheckSumParallel(path);
+    stopWatch.Stop();
+    Console.WriteLine($"Check sum for {path} using parallel method");
+    Console.WriteLine(BitConverter.ToString(result));
+    Console.WriteLine($"Time spent {stopWatch.Elapsed}");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine($"Cannot find {path}");
+}
+catch (IOException ex)
+{
+    Console.WriteLine(ex.Message);
+}
