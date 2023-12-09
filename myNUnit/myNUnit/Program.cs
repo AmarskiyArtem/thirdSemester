4b265e32243f7e7d3ec86ea93c6829f2ca9e051d
Artem <amarskiywork@gmail.com>
2023-12-09 17:13:42 +0300

    wip

--- myNUnit/myNUnit/Program.cs
+++ myNUnit/myNUnit/Program.cs
@@ -1,10 +1,19 @@
-﻿using myNUnit;
+﻿using System.Reflection;
+using myNUnit;
 
-if (args.Length != 1)
+/*if (args.Length != 1)
 {
     Console.WriteLine("Enter the path to test project");
 }
 else
 {
     
+}*/
+var a = Assembly.LoadFrom(@"C:\myFiles\programming\university\thirdSemester\matrixMultiplication\matrixMultiplication\bin\Debug\net8.0\matrixMultiplication.dll");
+Type[] types = a.GetTypes();
+
+// Выводим информацию о каждом найденном классе
+foreach (Type type in types)
+{
+    Console.WriteLine("Найден класс: " + type.FullName);
 }
